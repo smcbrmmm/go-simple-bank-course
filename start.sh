6c0e3d612f079902f59b70f128a47137d48ca587
smcbrmmm <samutch19@gmail.com>
2023-03-28 23:10:55 +0700

    update docker file

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@
 set -e
 
 echo "run db migration"
-/app/migrate -path /app/miration -database "$DB_SOURCE" -verbose up
+echo "samut"
+/app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
 exec "$@"
